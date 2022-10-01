23786eb6a5235d73f021c65e5a808c30eafa4c16
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-09-30 23:44:42 -0500

    update vite again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://hackathon2022-production.up.railway.app/',
+  base: 'https://hackathon2022-production.up.railway.app/hackathon2022/',
   plugins: [react()]
 })
