8374c6b57f49a7887d250327d31cd81f1effb649
jacoobes <jacobnguyend@gmail.com>
2022-10-01 17:11:09 -0500

    fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,7 @@ function NavBar() {
     return (
             <div className={"w3-bar w3-border-blue"}>
                 <a href="/" className={"w3-bar-item w3-hover-amber"}>Home</a>
+                <a href="#/login" className={"w3-bar-item w3-hover-amber"}>Login</a>
             </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './components/index.css'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 import BalChart from "./components/BalChart.jsx";
+import Login from "./components/Login.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter forceRefresh={true}>
+      <HashRouter forceRefresh={true}>
           <Routes>
-              <Route path="/hackathon2022" element={<App />}/>
-              <Route path="/hackathon2022/user-data" element={<BalChart />}></Route>
+              <Route path="/" element={<App />}/>
+              <Route path="user-data" element={<BalChart />}></Route>
+              <Route path="login" element={<Login />}></Route>
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 )
 
