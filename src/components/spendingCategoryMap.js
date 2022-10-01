e5670fcce5adf3d3ebb2cddb33c6345288a03c00
realjpower2002 <88989064+realjpower2002@users.noreply.github.com>
2022-10-01 15:14:59 -0500

    adding functionality to the category mapping class

--- src/components/spendingCategoryMap.js
+++ src/components/spendingCategoryMap.js
@@ -8,69 +8,74 @@ class spendingCategoryMap {
         this.categories.push("Drug Stores");
         this.categories.push("Vending Machines");
 
-        // declares a huge map that relates stores listed in
+        // huge map that relates stores listed in
         // transaction descriptions with expense categories
         //
+        // Default
+        this.spendingCategoryMap.set('','Other');
+        //
         // Groceries
-        this.spendingCategoryMap.set('',categories.at(0));
-        this.spendingCategoryMap.set('wal-mart',categories.at(1));
-        this.spendingCategoryMap.set('whole foods',categories.at(1));
-        this.spendingCategoryMap.set('target',categories.at(1));
-        this.spendingCategoryMap.set('aldi',categories.at(1));
-        this.spendingCategoryMap.set('trader joe',categories.at(1));
-        this.spendingCategoryMap.set('kroger',categories.at(1));
-        this.spendingCategoryMap.set('publix',categories.at(1));
-        this.spendingCategoryMap.set('wegman',categories.at(1));
-        this.spendingCategoryMap.set('safeway',categories.at(1));
-        this.spendingCategoryMap.set('costco',categories.at(1));
-        this.spendingCategoryMap.set('albertsons',categories.at(1));
-        this.spendingCategoryMap.set('h-e-b',categories.at(1));
-        this.spendingCategoryMap.set('hy-vee',categories.at(1));
-        this.spendingCategoryMap.set('winn-dixie',categories.at(1));
-        this.spendingCategoryMap.set('fresh market',categories.at(1));
+        this.spendingCategoryMap.set('wal-mart',"Groceries");
+        this.spendingCategoryMap.set('whole foods',"Groceries");
+        this.spendingCategoryMap.set('target',"Groceries");
+        this.spendingCategoryMap.set('aldi',"Groceries");
+        this.spendingCategoryMap.set('trader joe',"Groceries");
+        this.spendingCategoryMap.set('kroger',"Groceries");
+        this.spendingCategoryMap.set('publix',"Groceries");
+        this.spendingCategoryMap.set('wegman',"Groceries");
+        this.spendingCategoryMap.set('safeway',"Groceries");
+        this.spendingCategoryMap.set('costco',"Groceries");
+        this.spendingCategoryMap.set('albertsons',"Groceries");
+        this.spendingCategoryMap.set('h-e-b',"Groceries");
+        this.spendingCategoryMap.set('hy-vee',"Groceries");
+        this.spendingCategoryMap.set('winn-dixie',"Groceries");
+        this.spendingCategoryMap.set('fresh market',"Groceries");
         //
         // Fast Food/Restaurants
-        this.spendingCategoryMap.set('mcdonalds',categories.at(2));
-        this.spendingCategoryMap.set('starbucks',categories.at(2));
-        this.spendingCategoryMap.set('chick fil a',categories.at(2));
-        this.spendingCategoryMap.set('taco bell',categories.at(2));
-        this.spendingCategoryMap.set('wendy',categories.at(2));
-        this.spendingCategoryMap.set('dunkin',categories.at(2));
-        this.spendingCategoryMap.set('burger king',categories.at(2));
-        this.spendingCategoryMap.set('bk',categories.at(2));
-        this.spendingCategoryMap.set('subway',categories.at(2));
-        this.spendingCategoryMap.set('domino',categories.at(2));
-        this.spendingCategoryMap.set('chipotle',categories.at(2));
-        this.spendingCategoryMap.set('sonic',categories.at(2));
-        this.spendingCategoryMap.set('panera',categories.at(2));
-        this.spendingCategoryMap.set('pizza hut',categories.at(2));
-        this.spendingCategoryMap.set('kfc',categories.at(2));
-        this.spendingCategoryMap.set('popeye',categories.at(2));
-        this.spendingCategoryMap.set('panda express',categories.at(2));
-        this.spendingCategoryMap.set('dairy queen',categories.at(2));
-        this.spendingCategoryMap.set('little caesar',categories.at(2));
-        this.spendingCategoryMap.set('arby',categories.at(2));
-        this.spendingCategoryMap.set('olive garden',categories.at(2));
-        this.spendingCategoryMap.set('applebee',categories.at(2));
-        this.spendingCategoryMap.set('buffalo wild wings',categories.at(2));
-        this.spendingCategoryMap.set('papa john',categories.at(2));
+        this.spendingCategoryMap.set('mcdonalds',"Fast Food");
+        this.spendingCategoryMap.set('starbucks',"Fast Food");
+        this.spendingCategoryMap.set('chick fil a',"Fast Food");
+        this.spendingCategoryMap.set('taco bell',"Fast Food");
+        this.spendingCategoryMap.set('wendy',"Fast Food");
+        this.spendingCategoryMap.set('dunkin',"Fast Food");
+        this.spendingCategoryMap.set('burger king',"Fast Food");
+        this.spendingCategoryMap.set('bk',"Fast Food");
+        this.spendingCategoryMap.set('subway',"Fast Food");
+        this.spendingCategoryMap.set('domino',"Fast Food");
+        this.spendingCategoryMap.set('chipotle',"Fast Food");
+        this.spendingCategoryMap.set('sonic',"Fast Food");
+        this.spendingCategoryMap.set('panera',"Fast Food");
+        this.spendingCategoryMap.set('pizza hut',"Fast Food");
+        this.spendingCategoryMap.set('kfc',"Fast Food");
+        this.spendingCategoryMap.set('popeye',"Fast Food");
+        this.spendingCategoryMap.set('panda express',"Fast Food");
+        this.spendingCategoryMap.set('dairy queen',"Fast Food");
+        this.spendingCategoryMap.set('little caesar',"Fast Food");
+        this.spendingCategoryMap.set('arby',"Fast Food");
+        this.spendingCategoryMap.set('olive garden',"Fast Food");
+        this.spendingCategoryMap.set('applebee',"Fast Food");
+        this.spendingCategoryMap.set('buffalo wild wings',"Fast Food");
+        this.spendingCategoryMap.set('papa john',"Fast Food");
         //
         // Convenienve Stores
-        this.spendingCategoryMap.set('7-eleven',categories.at(3));
-        this.spendingCategoryMap.set('speedway',categories.at(3));
-        this.spendingCategoryMap.set('circle k',categories.at(3));
-        this.spendingCategoryMap.set('quiktrip',categories.at(3));
+        this.spendingCategoryMap.set('7-eleven',"Convenience Stores");
+        this.spendingCategoryMap.set('speedway',"Convenience Stores");
+        this.spendingCategoryMap.set('circle k',"Convenience Stores");
+        this.spendingCategoryMap.set('quiktrip',"Convenience Stores");
         //
         // Drugstore
-        this.spendingCategoryMap.set('cvs',categories.at(4));
-        this.spendingCategoryMap.set('walgreens',categories.at(4));
+        this.spendingCategoryMap.set('cvs',"Drug Stores");
+        this.spendingCategoryMap.set('walgreens',"Drug Stores");
         //
         // Misc
-        this.spendingCategoryMap.set('coca cola',categories.at(5));
+        this.spendingCategoryMap.set('coca cola',"Vending Machines");
     }
 
     //stores list of store category classifications
     #categories = [];
+
+    //stores list of stores
+    #stores = []
 
     //maps stores to store categories
     #spendingCategoryMap = new Map();
@@ -83,7 +88,6 @@ class spendingCategoryMap {
     addStoreToCategory(storeName, categoryName) {
         //finds index of category in the list from its name
         index = categories.findIndex(category => category === categoryName);
-
         //maps name to category
         this.spendingCategoryMap(storeName, category.at(index));
     }
@@ -92,25 +96,46 @@ class spendingCategoryMap {
         this.spendingCategoryMap.delete(storeName);
     }
 
+
     //adds a new category to the list (parameter takes the form)
     //of the name of the category
     addCategory(categoryName) {
         this.categories.push(categoryName);
     }
     deleteCategory(categoryName) {
-        i = 0
-        for(const[key,value] of this.#spendingCategoryMap) {
+        for(const[key,value] of this.spendingCategoryMap) {
             //this is super inefficient but it works well enough
+            //this prevents the order of the loop from being ruined
+            //if a deletion is made and the map moves up
             while(value === categoryName) {
                 spendingCategoryMap.delete(key);
             }
         }
-
         //finds index of category name
         index = categories.findIndex(category => category === categoryName);
-
         //deletes index of category name
-        this.categories.splice(categories.findIndex())
+        this.categories.splice(index)
+    }
+
+
+    getValue(key) {
+        this.spendingCategoryMap.get(key);
+    }
+
+    //a simple function to print the percentage composition of different
+    //categories in a list of transactions
+    printPercentages(Transactions) {
+        for(const[key,value] of this.spendingCategoryMap) {
+            for(constant[i] of Transactions) {
+                if(Transactions[i].getDescription().includes(key)) {
+                    numberOfTransactionsInCategory[spendingCategoryMap.findIndex(item => item === value)]++;
+                }
+            }
+        }
+        total = transactions.size();
+        for(const[i] of this.numberOfTransactionsInCategory) {
+            console.log(numberOfTransactionsInCategory[i]/total + " ");
+        }
     }
 };
 
